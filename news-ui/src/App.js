079375d295f2583b86d06776e0d4aec94fcb678e
Salmaan Ali <salmaan.arif.ali@gmail.com>
2024-04-11 18:10:17 -0400

    create buttons for gettting news

--- news-ui/src/App.js
+++ news-ui/src/App.js
@@ -3,21 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body>
+        <button className="button">Global News</button>
+        <button className="button">National News</button>
+        <button className="button">State News</button>
+      </body>
     </div>
   );
 }
